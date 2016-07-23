a3317f5d6bb1322c8663d28afe0ea22cf1d60d74
Duminda Kaviranga <kaviranga5955@gmail.com>
2016-07-23 14:58:28 +0530

    Second commit

---
+++ my_class/Class1.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace my_class
+{
+    public class Class1
+    {
+
+        public static double addfunc(double num1, double num2)
+        {
+
+            return (num1 + num2);
+
+        }
+
+        public static double subfunc(double num1, double num2)
+        {
+
+            return (num1 - num2);
+
+        }
+
+        public static double divfunc(double num1, double num2)
+        {
+
+            return num1/num2;
+
+        }
+
+        public static double mulfunc(double num1, double num2)
+        {
+
+            return num1*num2;
+
+        }
+
+    }
+}
